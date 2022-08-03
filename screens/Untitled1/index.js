084a4189248860f01f10388ceff46445a0837086
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-08-03 13:19:51 +0530

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,4 @@
+import { Pressable } from "react-native";
 import { ActivityIndicator } from "react-native";
 import React from "react";
 import { View } from "react-native";
@@ -10,12 +11,23 @@ const Untitled1 = () => {
     height: '100%'
   }}>
       <ActivityIndicator style={{
-      left: 81,
-      top: 109,
+      left: 99,
+      top: 122,
       position: "absolute",
       width: 50,
-      height: 50
-    }}></ActivityIndicator></View>;
+      height: 50,
+      backgroundColor: "#9e2323",
+      color: "#3aba94"
+    }}></ActivityIndicator><Pressable onPress={{}}><View style={{
+        left: 37,
+        top: 237,
+        position: "absolute",
+        height: 60,
+        width: 140,
+        backgroundColor: "#A54444",
+        borderRadius: 0,
+        color: "#777777"
+      }}></View></Pressable></View>;
 };
 
 export default Untitled1;
